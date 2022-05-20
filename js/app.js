31c9c038ab7baccde9d78cddf8dfd4c8a45951bd
Nenad <68515352+Nenad95M@users.noreply.github.com>
2022-05-20 16:17:10 +0200

    zavrseno, jos nesto hocu da ispravim

--- js/app.js
+++ js/app.js
@@ -1,25 +1,36 @@
 //globalno definisana sirina progressa
-let width=0;
+let width = 0;
+const circles = document.querySelectorAll('.circle');
+let currentActive = 1;
+
 //uvecanje progresa
-document.getElementById('next').addEventListener('click',function(){
-    if (width>=99){
-        return
+document.getElementById('next').addEventListener('click', function () {
+    if (currentActive != 4) {
+        circles[currentActive].classList.add('active');
+        currentActive++;
     }
-    width+=33;
-    changeProgress(width);
-}
-)
+    // console.log(currentActive);
+    if (width !=99) {
+        width += 33;
+        changeProgress(width);
+    }
+})
+
 //umanjenje progresa
-document.getElementById('prev').addEventListener('click',function(){
-    if (width<=0){
-        return
-    }   
-    width-=33;
-    changeProgress(width);
+document.getElementById('prev').addEventListener('click', function () {
+    if (currentActive != 1) {
+        currentActive--;
+        circles[currentActive].classList.remove('active');
+    }
+    // console.log(currentActive);
+    if (width != 0) {
+        width -= 33;
+        changeProgress(width);
+    }
 })
 
 //funkcija koja menja sirinu progresa
-function changeProgress(widthSize){
-
-    document.querySelector('.progress').style.width=`${widthSize}%`;
+function changeProgress(widthSize) {
+    document.querySelector('.progress').style.width = `${widthSize}%`;
 }
+
